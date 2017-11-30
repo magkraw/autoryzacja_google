0b3f9716c5dea6d49aa1167308dc841b2fac14a4
Magda Krawczyk <magdalena.a.krawczyk@gmail.com>
2017-11-30 14:08:11 +0100

    Autoryzacja

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  'GOOGLE_CLIENT_ID':'813620813548-22jape2gua0p2dvrqveuahqqih9504q0.apps.googleusercontent.com',
+  'GOOGLE_CLIENT_SECRET': 'dHU4FrFCeY23fmV8ig4ZvUND',
+  'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
